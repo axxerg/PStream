8886b5ceec02bc2104b6fd45ebf89d1d847f54ff
axxerg <worldmediaman@gmail.com>
2026-05-18 20:51:59 +0200

    Create token.py

---
+++ atv/token.py
@@ -0,0 +1,23 @@
+import requests
+import re
+
+url = "https://www.atvavrupa.tv/canli-yayin"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+html = requests.get(url, headers=headers).text
+
+match = re.search(r'https://.*?\.m3u8\?st=.*?&e=\d+', html)
+
+if match:
+    stream = match.group(0)
+
+    with open("stream.txt", "w") as f:
+        f.write(stream)
+
+    print(stream)
+
+else:
+    print("Kein Stream gefunden")
